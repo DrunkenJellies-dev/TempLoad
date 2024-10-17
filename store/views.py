f3ada44ddb25572edde4811c2275eeeefe482b12
Callum Newman <ballunew@gmail.com>
2024-10-17 10:50:15 +0100

    Added Login functionality and alert popups

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -12,7 +12,22 @@ def about(request):
     return render(request, 'about.html', {})
 
 def loginUser(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You've been logged in."))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error, please try again."))
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
 
 def logoutUser(request):
-    pass
+    logout(request)
+    messages.success(request, ("You've been logged out."))
+    return redirect('home')
