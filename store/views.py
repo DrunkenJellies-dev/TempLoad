743116de852f4e5e2d99f655101d5c63251ca604
Callum Newman <ballunew@gmail.com>
2024-10-17 10:13:09 +0100

    Added login and log out buttons along with pages and a check for is there is a user is logged in which will display either the login button of the logout button

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
     products = Product.objects.all()
@@ -7,3 +10,9 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def loginUser(request):
+    return render(request, 'login.html')
+
+def logoutUser(request):
+    pass
