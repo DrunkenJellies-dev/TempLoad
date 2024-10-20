447b1cec73fa65734e3893ce129a9ce95a679f38
CallumNewman <ballumnew@gmail.com>
2024-10-20 16:51:13 +0100

    Extended the user model with a new profile model for address and phone number

--- store/models.py
+++ store/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
+# Create custom profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=50, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    county = models.CharField(max_length=200, blank=True)
+    postcode = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+# Create a user profile by default when a user signs in
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        userProfile = Profile(user=instance)
+        userProfile.save()
+
+# Automate the profile creation
+post_save.connect(createProfile, sender=User)
 
 # Category Model
 class Category(models.Model):
