d18511170de5b82b89cfb9c957a4725fbd281422
CallumNewman <ballumnew@gmail.com>
2024-10-21 22:28:05 +0100

    Created the shipping address model in the new payment app

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    fullName = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    county = models.CharField(max_length=255, null=True, blank=True)
+    postcode = models.CharField(max_length=255, null=True, blank=True)
+    fullName = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    
+
+    #Don't pluralize address
+    class Meta:
+        verbose_name_plural = "ShippingAddress"
+
+    def __str__(self):
+        return f'ShippingAddress - {str(self.id)}'
