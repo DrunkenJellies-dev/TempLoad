dde37ee5e084db239ff5e130f37d0e1e8a9b8e9c
CallumNewman <ballumnew@gmail.com>
2024-10-22 00:10:56 +0100

    Created the Order Model

--- payment/models.py
+++ payment/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
+# Shipping Address Model
 class ShippingAddress(models.Model):
+    # Foreign Key
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
     shippingFullName = models.CharField(max_length=255)
     shippingEmail = models.CharField(max_length=255)
     shippingAddress1 = models.CharField(max_length=255)
@@ -19,3 +23,19 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f'ShippingAddress - {str(self.id)}'
+    
+# Order Model
+class Order(models.model):
+    # Foreign Key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    fullName = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    shippingAddress = models.TextField(max_length=15000)
+    amountPaid= models.DecimalField(max_digits=10, decimal_places=2)
+    dateOrdered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+    
+  
