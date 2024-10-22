9c6aee89762eb0018d8e3065bcbb7e3b675fe932
Callum Newman <ballunew@gmail.com>
2024-10-22 15:25:54 +0100

    Added shipping form to the checkout page

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from cart.cart import Cart
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
 
 def checkout(request):
     # Get the cart
@@ -8,7 +10,19 @@ def checkout(request):
     cartProducts = cart.getProducts
     quantities = cart.getQuantities
     total = cart.cartTotal()
-    return render(request, 'payment/checkout.html', {"cartProducts":cartProducts, "quantities":quantities, "total":total})
+
+    if request.user.is_authenticated:
+          # Checkout as logged in user
+
+          #Shipping User
+          shippingUser = ShippingAddress.objects.get(user__id=request.user.id)
+          
+          #Shipping Form
+          shippingForm = ShippingForm(request.POST or None, instance=shippingUser)
+          return render(request, "payment/checkout.html", {"cartProducts":cartProducts, "quantities":quantities, "total":total, "shippingForm":shippingForm })
+    else:
+          shippingForm = ShippingForm(request.POST or None)
+          return render(request, "payment/checkout.html", {"cartProducts":cartProducts, "quantities":quantities, "total":total, "shippingForm":shippingForm })
 
 def paymentSuccess(request):
     return render(request, "payment/paymentSuccess.html")
