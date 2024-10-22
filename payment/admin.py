0a824a61904a40f6d3c4d0f75182ffc6eea14973
CallumNewman <ballumnew@gmail.com>
2024-10-23 00:50:49 +0100

    Added Inline OrderItems display for the Order model in the admin section

--- payment/admin.py
+++ payment/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 # Register Model In the Admin System
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+# Create order item inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+# Extend Order Model
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inline = [OrderItemInline]
+
+# Unregister order model
+admin.site.unregister(Order)
+
+# Re-register our Order and order items
+admin.site.register(Order,OrderAdmin)
