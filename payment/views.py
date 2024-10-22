a326431f69644e1bde8e75e5efa8947dc8fce0a5
Callum Newman <ballunew@gmail.com>
2024-10-22 14:30:28 +0100

    Added initial checkout page

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def checkout(request):
+    return render(request, "payment/checkout.html")
+
 def paymentSuccess(request):
     return render(request, "payment/paymentSuccess.html")
