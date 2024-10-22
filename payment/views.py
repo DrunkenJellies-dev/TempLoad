d63b06629b7edb649e29c0c38bd91a81f57c66e7
Callum Newman <ballunew@gmail.com>
2024-10-22 15:03:48 +0100

    Created view for the checkout page and displayed all of the products in the basket on the page

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from cart.cart import Cart
 
 def checkout(request):
-    return render(request, "payment/checkout.html")
+    # Get the cart
+    cart = Cart(request)
+    # Get the products from the cart
+    cartProducts = cart.getProducts
+    quantities = cart.getQuantities
+    total = cart.cartTotal()
+    return render(request, 'payment/checkout.html', {"cartProducts":cartProducts, "quantities":quantities, "total":total})
 
 def paymentSuccess(request):
     return render(request, "payment/paymentSuccess.html")
