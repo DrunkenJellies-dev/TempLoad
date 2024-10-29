e147ba46bd766f245b3396a2ddace1070ad08dc6
CallumNewman <ballumnew@gmail.com>
2024-10-29 19:58:02 +0000

    Modified where the database setting in settings.py

--- TempLoad/settings.py
+++ TempLoad/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'TempLoad.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL") )
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -116,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
